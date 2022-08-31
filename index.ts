7722af9d7c7f90729dbcc2d4e384c31795380a98
Animechill30 <82389963+AmerUkmata@users.noreply.github.com>
2022-08-31 22:47:44 +0200

    a

---
+++ index.ts
@@ -0,0 +1,57 @@
+import express from 'express'
+import cors from 'cors'
+
+const quotes = [
+    {
+        id: 1,
+        user: {
+            username: "Amer",
+            gamertag: "H3LL G",
+            age: 18,
+        },
+        quote: "welcome to H3LL"
+    },
+    {
+        id: 2,
+        user: {
+            username: "Amer",
+            gamertag: "H3LL G",
+            age: 18,
+        },
+        quote: "welcome to H3LL"
+    },
+    {
+        id: 3,
+        user: {
+            username: "Amer",
+            gamertag: "H3LL G",
+            age: 18,
+        },
+        quote: "welcome to H3LL"
+    },
+    {
+        id: 4,
+        user: {
+            username: "Amer",
+            gamertag: "H3LL G",
+            age: 18,
+        },
+        quote: "welcome to H3LL"
+    }
+]
+
+const app = express()
+app.use(cors())
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Nope')
+})
+
+app.get('/quotes', (req, res) => {
+    let quotesDoSend = quotes
+    if(req.query.age) {
+      quotesDoSend = quotesDoSend.filter(quote => quote.user.age === Number(req.query.age))
+    }
+    res.send(quotesDoSend)
+})
